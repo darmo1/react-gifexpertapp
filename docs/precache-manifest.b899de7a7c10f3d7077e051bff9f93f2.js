eb599471895fcd0627b5b7e1ddb41b604c657223
Danilo Morales <darmito10@gmail.com>
2020-08-16 22:36:14 -0500

    docs agregado

---
+++ docs/precache-manifest.b899de7a7c10f3d7077e051bff9f93f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63b8e4a308cfbb97ec8ed0245a7d205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2338e13c6722f9f16c4",
+    "url": "/static/css/main.26bfcca9.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2338e13c6722f9f16c4",
+    "url": "/static/js/main.20218404.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
